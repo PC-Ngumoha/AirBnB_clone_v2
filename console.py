390f5e9a925dedcade565c10d38be828c8e3c8ff
PC-Ngumoha <pcngumoha@gmail.com>
2023-01-02 09:32:50 +0000

    Features Update: Update 'create' command for the console to work with CLI parameters

--- console.py
+++ console.py
@@ -51,7 +51,7 @@ class HBNBCommand(cmd.Cmd):
             pline = line[:]  # parsed line
 
             # isolate <class name>
-            _cls = pline[:pline.find('.')]
+            _cls = pline[:]
 
             # isolate and validate <command>
             _cmd = pline[pline.find('.') + 1:pline.find('(')]
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -113,15 +113,33 @@ class HBNBCommand(cmd.Cmd):
         """ Overrides the emptyline method of CMD """
         pass
 
-    def do_create(self, args):
+    def do_create(self, line):
         """ Create an object of any class"""
-        if not args:
+        if not line:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
-            print("** class doesn't exist **")
-            return
-        new_instance = HBNBCommand.classes[args]()
+        else:
+            pline = line[:]
+            _cls = pline[:pline.find(' ')]  # Gets the class name
+            if _cls not in HBNBCommand.classes:
+                print("** class doesn't exist **")
+                return
+            pline = pline[pline.find(' ') + 1:]
+            params = pline.split(' ')
+            args = {}
+            for param in params:
+                param = param.split('=')
+                key = param[0]
+                value = param[1]
+                if value.__contains__('"'):
+                    value = value.replace('"', '').replace('_', ' ')
+                elif value.__contains__("."):
+                    value = float(value)
+                else:
+                    value = int(value)
+                args.update({key: value})
+        new_instance = HBNBCommand.classes[_cls]()
+        new_instance.__dict__.update(args)
         storage.save()
         print(new_instance.id)
         storage.save()
@@ -187,7 +205,7 @@ class HBNBCommand(cmd.Cmd):
         key = c_name + "." + c_id
 
         try:
-            del(storage.all()[key])
+            del storage.all()[key]
             storage.save()
         except KeyError:
             print("** no instance found **")
@@ -272,7 +290,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +298,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
@@ -319,6 +337,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
