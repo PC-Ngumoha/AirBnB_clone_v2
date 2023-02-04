82644ac89481b286fcce61960670de493217c64e
PC-Ngumoha <pcngumoha@gmail.com>
2023-02-04 15:12:54 +0000

    Task Update: Create '2-c_route.py' app

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Displays 'Hello HBNB!' on the web page using Flask"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """ Hello HBNB! """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ HBNB """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """ C is fun """
+    return "C {}".format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
