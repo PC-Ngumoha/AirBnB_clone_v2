1aff43c4cc3e44902ee6c66ba34bf63bda3ef945
PC-Ngumoha <pcngumoha@gmail.com>
2023-02-04 21:21:44 +0000

    Task Update: Complete 'web_flask/4-number_route.py' app

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Displays 'Hello HBNB!' on the web page using Flask"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """ Hello HBNB! """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ HBNB """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """ C is fun """
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text="is_cool"):
+    """ Python is cool """
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """ n is a number """
+    return "{} is a number".format(n)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
