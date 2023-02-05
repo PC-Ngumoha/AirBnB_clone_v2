63f44c3f8f943357c7c47d78374c9a2e14eb3184
PC-Ngumoha <pcngumoha@gmail.com>
2023-02-05 20:07:03 +0000

    Features Update: Add 'close' functionality to 'FileStorage' and 'DBStorage' storage engines

--- test.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""Testing things out """
-from fabric.api import *
-
-def list_directory(archive_folder):
-    result = local('ls versions/', capture=True)
-    print(result.split('\n'))
