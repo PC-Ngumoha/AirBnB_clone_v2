9a175afc70d3f30c15e1a9069b3cec7ca67d05b2
PC-Ngumoha <pcngumoha@gmail.com>
2023-02-05 20:39:37 +0000

    Features Update: Create

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""This module instantiates a storage object"""
 import os
 from models.amenity import Amenity
 from models.city import City
@@ -7,17 +7,17 @@ from models.state import State
 from models.place import Place
 from models.review import Review
 from models.user import User
+from models.engine.db_storage import DBStorage
+from models.engine.file_storage import FileStorage
+
 
 store_type = os.getenv('HBNB_TYPE_STORAGE')
 
 if store_type == 'db':
-    from models.engine.db_storage import DBStorage
     storage = DBStorage()
-    storage.reload()
 else:
-    from models.engine.file_storage import FileStorage
     storage = FileStorage()
-    storage.reload()
+storage.reload()
 
 # Enables us perform wildcard package importation
 __all__ = ['Amenity', 'City', 'State', 'Place', 'Review', 'User', 'storage']
